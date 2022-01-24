277788e753ee77bb64f612ae16083bb316db2fef
EikoNakashima <e.nakashima@gemcook.com>
2022-01-24 11:14:54 +0900

    fix: main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
+)
+
+type User struct {
+	Id   int64
+	Name string
+	Age  int
+}
+
+func main() {
+	engine, err := xorm.NewEngine("mysql", "root:root@tcp([127.0.0.1]:3306)/sample_db?charset=utf8mb4&parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("うまく動きました。")
+
+}
