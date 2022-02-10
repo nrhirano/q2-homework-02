3639bef105cc0a630db87f14e9a7b4ec899911fa
nrhirano <ryuuji.hirano@nishio-rent.co.jp>
2022-02-10 15:41:08 +0900

    feat : NG_BACK_END-176

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.17.0-alpine3.14
+
+ENV PATH=/usr/local/git/bin:$PATH
+
+RUN apk add --no-cache git
+
+WORKDIR ${PATH}
+
+COPY ["./go.mod","./main.go", "/"]
+
+RUN go mod download
+
+RUN go mod tidy
+
+CMD go run /main.go
--- main.go
+++ main.go
@@ -9,17 +9,48 @@ import (
 )
 
 type User struct {
-	Id   int64
-	Name string
-	Age  int
+	Id   int64  `xorm:"id pk"`
+	Name string `xorm:"name"`
+	Age  int    `xorm:"age"`
+}
+
+func Insert(engine xorm.Engine) {
+	user := User{
+		Id:   1,
+		Name: "tanaka",
+		Age:  20,
+	}
+
+	_, err := engine.Table("user").Insert(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("< Insert > ID:%d 名前:%s, 年齢:%d\n", user.Id, user.Name, user.Age)
+}
+
+func Get(engine xorm.Engine) {
+	user := User{}
+	result, err := engine.Where("Id = ?", 1).Get(&user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !result {
+		log.Fatal("Not Found")
+	}
+	fmt.Printf("< Get > ID:%d 名前:%s, 年齢:%d\n", user.Id, user.Name, user.Age)
 }
 
 func main() {
-	engine, err := xorm.NewEngine("mysql", "root:root@tcp(db:3306)/sample_db?charset=utf8mb4&parseTime=true")
+	engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/sample_db?charset=utf8mb4&parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Hello World!")
 
+	Insert(*engine)
+	Get(*engine)
+
+	defer engine.Close()
 }
