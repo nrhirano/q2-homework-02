d7f8f31b61069d9dc9066e588a3f825a0f30ff71
nrhirano <ryuuji.hirano@nishio-rent.co.jp>
2022-02-16 11:44:48 +0900

    refactor : レビュー内容の修正

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,10 @@ ENV PATH=/usr/local/git/bin:$PATH
 
 RUN apk add --no-cache git
 
-WORKDIR ${PATH}
+WORKDIR PATH
 
 COPY ["./go.mod","./main.go", "/"]
 
 RUN go mod download
 
 RUN go mod tidy
-
-CMD go run /main.go
--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 )
 
 type User struct {
-	Id   int64  `xorm:"id pk"`
+	ID   int64  `xorm:"id pk autoincr"`
 	Name string `xorm:"name"`
 	Age  int    `xorm:"age"`
 }
 
 func Insert(engine xorm.Engine) {
 	user := User{
-		Id:   1,
+		ID:   1,
 		Name: "tanaka",
 		Age:  20,
 	}
@@ -26,29 +26,38 @@ func Insert(engine xorm.Engine) {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("< Insert > ID:%d 名前:%s, 年齢:%d\n", user.Id, user.Name, user.Age)
+	fmt.Printf("< Insert > ID:%d 名前:%s, 年齢:%d\n", user.ID, user.Name, user.Age)
 }
 
 func Get(engine xorm.Engine) {
 	user := User{}
-	result, err := engine.Where("Id = ?", 1).Get(&user)
+	result, err := engine.Where("ID = ?", 1).Get(&user)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if !result {
 		log.Fatal("Not Found")
 	}
-	fmt.Printf("< Get > ID:%d 名前:%s, 年齢:%d\n", user.Id, user.Name, user.Age)
+	fmt.Printf("< Get > ID:%d 名前:%s, 年齢:%d\n", user.ID, user.Name, user.Age)
+}
+
+func CreateTable(engine xorm.Engine) {
+	err := engine.CreateTables(User{})
+	if err != nil {
+		log.Fatalf("テーブルの生成に失敗しました。: %v", err)
+	}
+	fmt.Println("テーブル作成が成功しました。")
 }
 
 func main() {
-	engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/sample_db?charset=utf8mb4&parseTime=true")
+	engine, err := xorm.NewEngine("mysql", "root:root@tcp(db:3306)/sample_db?charset=utf8mb4&parseTime=true")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println("Hello World!")
 
+	CreateTable(*engine)
 	Insert(*engine)
 	Get(*engine)
 
